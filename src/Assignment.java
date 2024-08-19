c009b2780cbc9175b7c523f099986e86fb48971f
hernabra000 <hernabra000@warren.k12.in.us>
2024-08-19 13:13:23 -0400

    finishes barking assingment

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Assignment {
+public class Assignment {
+    public static void main(String[] args) {
+        shouldWakeUp(true, 1);
+        shouldWakeUp(false, 2);
+        shouldWakeUp(true, 8);
+        shouldWakeUp(true, -1);
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (hourOfDay < 0 || hourOfDay > 23){
+            return false;
+        }
+
+        if (barking == false){
+            return false;
+        }
+
+        if (hourOfDay < 8 || hourOfDay > 22 || barking == true){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
